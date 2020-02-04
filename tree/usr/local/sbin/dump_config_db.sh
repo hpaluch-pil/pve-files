a81e17da36fc453e258864dfa0c8ec6b38a32131
Henryk Paluch <henryk.paluch@pickering.cz>
2020-02-04 12:15:51 +0100

    Dump config.db regularly

---
+++ tree/usr/local/sbin/dump_config_db.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Dumps config.db as SQL commands
+# Written by Henryk Paluch
+
+set -e
+set -o pipefail
+
+db=/var/lib/pve-cluster/config.db
+
+[ $# -eq 1 ] || {
+	echo "Usage: $0 dump_directory" >&2
+	exit 1
+}
+
+d="$1"
+[ -d "$d" ] || {
+	echo "Specified path '$d' is not directory" >&2
+	exit 1
+}
+
+[ -r "$db" ] || {
+	echo "Database file '$db' unreadable" >&2
+	exit 1
+}
+
+t="$d/config-db-`date '+%Y-%m-%d-%H-%M-%S'`.sql"
+
+sqlite3 "$db" .dump > $t
+tail -1 $t | egrep -q "^COMMIT" || {
+	echo "Last line in dump '$t' must contain COMMIT" >&2
+	exit 1
+}
+gzip -9 $t
+
+exit 0
