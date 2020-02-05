b6ce7aa2329c7585a6fb6f51175a489d729bfa7c
Henryk Paluch <henryk.paluch@pickering.cz>
2020-02-05 15:01:14 +0100

    Script to show cgroups swapiness

---
+++ tree/usr/local/sbin/show_swappiness.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# show crgoups swappiness. Please see:
+# - https://forum.proxmox.com/threads/swappiness-question.42295/page-2#post-248010
+# - https://github.com/systemd/systemd/issues/9276#issuecomment-442514543
+
+set -e
+for i in /sys/fs/cgroup/memory/*/memory.swappiness
+do
+	echo -n "$i:"
+	cat $i
+done
+exit 0
